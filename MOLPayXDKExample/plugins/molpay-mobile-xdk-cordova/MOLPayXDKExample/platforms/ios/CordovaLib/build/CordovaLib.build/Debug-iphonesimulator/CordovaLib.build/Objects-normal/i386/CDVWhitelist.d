e91ea3e2055456035bb420a7f29c1b189af906c8
Clement <clement@MOLPays-MacBook-Pro.local>
2016-04-12 11:28:15 +0800

    1.0.4

---
+++ MOLPayXDKExample/plugins/molpay-mobile-xdk-cordova/MOLPayXDKExample/platforms/ios/CordovaLib/build/CordovaLib.build/Debug-iphonesimulator/CordovaLib.build/Objects-normal/i386/CDVWhitelist.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Volumes/DATA/Dev/molpay-mobile-xdk/private/Cordova/example/platforms/ios/CordovaLib/Classes/Public/CDVWhitelist.m \
+  /Volumes/DATA/Dev/molpay-mobile-xdk/private/Cordova/example/platforms/ios/CordovaLib/Classes/Public/CDVWhitelist.h
